8361862d035081fc04eecb1c52a9385905fc0e1b
MOHAMMAD JOBAEID HASAN <mdjobaeidhasan92@gmail.com>
2025-03-21 17:04:38 +0600

    Create changelogs.js

---
+++ public/changelogs.js
@@ -0,0 +1,34 @@
+let allLogs = document.querySelector(".allLogs")
+
+let getChangeLogs = async () => {
+
+  let owner = "MaheshTechnicals"
+  let repo = "Quotify-Styler-Cdn"
+
+  let res = await fetch(`https://api.github.com/repos/${owner}/${repo}/releases`)
+  let data = await res.json()
+  allLogs.innerHTML = ""
+
+  await Array.from(data).forEach((e) => {
+
+    let name = e.name
+    let description = e.body
+    let time = e.created_at.slice(0, 10)
+
+
+    let element = `<div class="log">
+        <h4 class="mb-3"><i class="fa-regular fa-gem"></i> ${name}</h4>
+        <p class="mb-5 text-info"><i class="fa-regular fa-clock text-white"></i> ${time}</p>
+        <pre class="text-light" >${description}</pre>
+      </div>`
+
+    allLogs.innerHTML += element
+
+  })
+
+
+
+
+}
+
+getChangeLogs()
